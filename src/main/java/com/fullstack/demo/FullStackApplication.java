27a1dc833f81b5e55f8b2ed5fc90eb2c2208cb9b
ahmedeldeeb <ahmedeldeeb5050@outlook.com>
2018-08-21 14:29:24 +0200

    Initial Commit

---
+++ src/main/java/com/fullstack/demo/FullStackApplication.java
@@ -0,0 +1,12 @@
+package com.fullstack.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackApplication.class, args);
+    }
+}
